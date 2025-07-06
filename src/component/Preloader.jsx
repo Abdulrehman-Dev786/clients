69d8d9e8544c7accb355c3b8c2d5e88ab77e0ffc
hammad <hammad>
2025-07-06 20:33:33 +0500

    ✨ Added working preloader with cube animation

--- src/component/Preloader.jsx
+++ src/component/Preloader.jsx
@@ -1,16 +1,16 @@
-import './Preloader.css';
-
+import React from "react";
+import "./Preloader.css";
 
 const Preloader = () => {
   return (
     <div className="preloader">
       <div className="cube">
-        <div className="items-center content-center font-bold  w-full h-full text-1xl face front ">CODE <br /><span>NCE</span></div>
-        <div className="items-center content-center font-bold  w-full h-full text-1xl  face back">CODE <br /><span>NCE</span></div>
-        <div className="items-center content-center font-bold  w-full h-full text-1xl  face right">CODE <br /><span>NCE</span></div>
-        <div className="items-center content-center font-bold  w-full h-full text-1xl  face left">CODE <br /><span>NCE</span></div>
-        <div className="items-center content-center font-bold  w-full h-full text-1xl  face top">CODE <br /><span>NCE</span></div>
-        <div className="items-center content-center font-bold  w-full h-full text-1xl  face bottom">CODE <br /><span>NCE</span></div>
+        <div className="cube-face front">CODE <br /><span>NCE</span></div>
+        <div className="cube-face back">CODE <br /><span>NCE</span></div>
+        <div className="cube-face right">CODE <br /><span>NCE</span></div>
+        <div className="cube-face left">CODE <br /><span>NCE</span></div>
+        <div className="cube-face top">CODE <br /><span>NCE</span></div>
+        <div className="cube-face bottom">CODE <br /><span>NCE</span></div>
       </div>
     </div>
   );
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,43 +9,10 @@ import Footer from '../component/footer';
 import Contact from '../component/contact';
 import Preloader from '../component/Preloader';
 
-// function Home() {
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     const timer = setTimeout(() => setLoading(false), 2000);
-//     return () => clearTimeout(timer);
-//   }, []);
-
-//   if (loading) {
-//     return (
-//       <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 animate-pulse">
-//         <div className="text-6xl mb-4">🚧</div>
-//         <div className="text-4xl font-bold text-white mb-2">Sorry! Site Under Maintenance</div>
-//         <div className="text-xl text-white">Some features might not work properly right now.<br/>Please wait while we prepare something amazing for you! 🌟</div>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <>
-//       <Navbar />
-//       <Hero />
-//       <About />
-//       <Skills />
-//       <Project />
-//       <Hireme />
-//       <Contact />
-//       <Footer />
-//     </>
-//   );
-// }
-
 function Home() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Simulate loading time (e.g. 3 seconds)
     const timer = setTimeout(() => {
       setLoading(false);
     }, 3000);
@@ -58,23 +25,19 @@ function Home() {
       {loading ? (
         <Preloader />
       ) : (
-        <div>
-          {/* Your main website starts here */}
-          
-        </div>
+        <>
+          <Navbar />
+          <Hero />
+          <About />
+          <Skills />
+          <Project />
+          <Hireme />
+          <Contact />
+          <Footer />
+        </>
       )}
-       <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Project />
-      <Hireme />
-      <Contact />
-      <Footer />
     </>
   );
 }
-
-
 
 export default Home;
